20040ee7ddc500068c17cacc884d300a98285184
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-22 08:24:19 -0600

    Funciones agregadas de JQuery, seleccion de elementos required

--- js/actions.js
+++ js/actions.js
@@ -1,20 +1,6 @@
-function saluda(){
-  console.log('Hola que tal todo');
-}
+//Obtener los botones tipo submit
+let submitButton = $('button[type=submit]');
+//console.log(submitButton);
 
-
-// Obtener input Username
-let input = document.getElementById('username');
-let signButton = document.getElementById('signup');
-
-// Se agrega el evento onFocus
-input.addEventListener('focus',saluda);
-input.addEventListener('blur',function() {
-  console.log('Me sali del input');
-  console.log(this.value); // Puede servir para validar
-});
-
-signButton.addEventListener('click',function() {
-  console.log(this); // Imprime todo
-  console.log(this.innerHTML); // Imprime el texto que hay en el html
-});
+//Agregar el listener para el submit
+submitButton.on('click',validateForm);
--- js/valida.js
+++ js/valida.js
@@ -1,1 +1,26 @@
-console.log("hola");
+//console.log($('input'));
+let validateForm = function validateForm(e){
+  let $button = $(this);
+  //Obtain form
+  let $form = $button.parent('form');
+
+  //Obtatin required elements
+  let $required = $form.find('*[required]'); //Input / *
+
+  validateRequiredFields($required);
+
+  //Obtain any other element
+
+  //Validate required elements
+
+  e.preventDefault(); //Don't execute
+
+}
+
+let validateRequiredFields = function validateRequiredFields(fields){
+    console.log(fields);
+    $.each(fields, function() {
+      let element = $(this);
+      console.log(element);
+    });
+}
